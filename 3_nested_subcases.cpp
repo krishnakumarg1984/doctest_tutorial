864ad4f803d62b310ca215621165e9309dd79dc7
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-01-22 23:44:06 +0000

    adds the subcases examples

---
+++ 3_nested_subcases.cpp
@@ -0,0 +1,44 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <iostream>
+
+#include "doctest/doctest.h"
+
+TEST_CASE("lots of nested subcases") {
+    std::cout << '\n' << "root" << '\n';
+    SUBCASE("") {
+        std::cout << "1" << '\n';
+        SUBCASE("") { std::cout << "1.1" << '\n'; }
+    }
+    SUBCASE("") {
+        std::cout << "2" << '\n';
+        SUBCASE("") { std::cout << "2.1" << '\n'; }
+        SUBCASE("") {
+            std::cout << "2.2" << '\n';
+            SUBCASE("") {
+                std::cout << "2.2.1" << '\n';
+                SUBCASE("") { std::cout << "2.2.1.1" << '\n'; }
+                SUBCASE("") { std::cout << "2.2.1.2" << '\n'; }
+            }
+        }
+        SUBCASE("") { std::cout << "2.3" << '\n'; }
+        SUBCASE("") { std::cout << "2.4" << '\n'; }
+    }
+    SUBCASE("") {
+        std::cout << "1" << '\n';
+        SUBCASE("") { std::cout << "1.1" << '\n'; }
+    }
+    SUBCASE("") {
+        std::cout << "2" << '\n';
+        SUBCASE("") { std::cout << "2.1" << '\n'; }
+        SUBCASE("") {
+            std::cout << "2.2" << '\n';
+            SUBCASE("") {
+                std::cout << "2.2.1" << '\n';
+                SUBCASE("") { std::cout << "2.2.1.1" << '\n'; }
+                SUBCASE("") { std::cout << "2.2.1.2" << '\n'; }
+            }
+        }
+        SUBCASE("") { std::cout << "2.3" << '\n'; }
+        SUBCASE("") { std::cout << "2.4" << '\n'; }
+    }
+}
